daa3707e458c3d2780860d79ccb2e37843b6da45
Pastis444 <67665527+Pastis444@users.noreply.github.com>
2023-02-14 00:05:56 +0100

    Updated branch name.

--- DumbWRLD.lua
+++ DumbWRLD.lua
@@ -11,9 +11,9 @@ _G.DumbWRLDState = true
 
 -- API CALLS
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/main/bracketv3.lua"))()
-local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/main/xlpapi.lua"))()
-local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/main/bssapi.lua"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/master/bracketv3.lua"))()
+local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/master/xlpapi.lua"))()
+local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pastis444/DumbWRLD/master/bssapi.lua"))()
 
 if not isfolder("DumbWRLD") then makefolder("DumbWRLD") end
 if isfile('DumbWRLD.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "fU3jdejJxK"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile('DumbWRLD.txt', "discord")})end
